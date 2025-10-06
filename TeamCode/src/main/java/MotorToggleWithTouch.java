8d3bcecdf9b6bf97d3327c8bb873d559999653b3
rusha11y <rush.242@gmail.com>
2025-10-06 14:42:07 -0400

    StarterBot 2025 Autonomous + TeleOp

--- TeamCode/src/main/java/MotorToggleWithTouch.java
+++ TeamCode/src/main/java/MotorToggleWithTouch.java
@@ -1,24 +1,48 @@
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
-@TeleOp(name = "Touch Sensor Debug", group = "Test")
-public class TouchSensorDebug extends LinearOpMode {
+@TeleOp(name = "Motor Toggle with Touch", group = "Example")
+public class MotorToggleWithTouch extends LinearOpMode {
+
+    private DcMotor gearMotor;
     private TouchSensor touchSensor;
+
+    private boolean motorOn = false;
+    private boolean lastTouchState = false;
 
     @Override
     public void runOpMode() {
+        gearMotor = hardwareMap.get(DcMotor.class, "gearmotor");
         touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
 
-        telemetry.addData("Status", "Waiting for start");
+        gearMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
-            telemetry.addData("Touch Pressed", touchSensor.isPressed());
+            boolean isPressed = touchSensor.isPressed(); // true when pressed
+
+            // Detect rising edge: just pressed
+            if (isPressed && !lastTouchState) {
+                motorOn = !motorOn;
+
+                gearMotor.setPower(motorOn ? 0.5 : 0.0);
+            }
+
+            telemetry.addData("Touch Sensor", isPressed ? "Pressed" : "Not Pressed");
+            telemetry.addData("Motor State", motorOn ? "ON" : "OFF");
             telemetry.update();
-            sleep(100);
+
+            lastTouchState = isPressed;
+
+            sleep(20);
         }
+
+        gearMotor.setPower(0); // stop motor when opmode ends
     }
 }
